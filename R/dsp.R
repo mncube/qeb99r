7e6426892ae0d110729c7eff62c1c43006e2bfb4
Mackson Ncube <macksonncube@gmail.com>
2021-12-05 09:08:32 -0800

    document dsp function

--- R/dsp.R
+++ R/dsp.R
@@ -1,4 +1,19 @@
-dsp <- function(data, species, distance, ...){
+#' dsp
+#'
+#' Density of species (Dsp) function
+#'
+#' @param data a data frame
+#' @param species data frame column specifying the species of the observation
+#' @param distance distance from random sampling point to nearest individual of a species
+#'
+#' @return input data frame with Dsp (density of species) column added on
+#' @export
+#'
+#' @examples
+#' df1 <- data.frame(sp = c("a", "b", "b", "a", "b", "a"),
+#'                   dis = c(3, 6, 4, 7, 3, 8))
+#' dsp(df1, sp, dis)
+dsp <- function(data, species, distance){
 
   #Use evaluation methods
   species <- deparse(substitute(species))
@@ -10,7 +25,7 @@ dsp <- function(data, species, distance, ...){
 
   #Do calculation
   temp <- dplyr::group_by(temp, species)
-  data <- dplyr::ungroup(dplyr::mutate(temp, dsp = ((mean(distance)^2)/2)^(1/2)))
+  data <- dplyr::ungroup(dplyr::mutate(temp, Dsp = ((mean(distance)^2)/2)^(1/2)))
 
   #Return data frame with dsps
   return(data)
